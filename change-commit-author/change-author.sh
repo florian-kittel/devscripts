258cce116a1ed4abf39004a030f2a7e3e2b923af
florian-kittel <mail@florian-kittel.de>
2018-02-03 09:16:51 +0100

    feat(change-author): add script for changing git author and email in repo

---
+++ change-commit-author/change-author.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+git filter-branch --env-filter '
+OLD_EMAIL="my@wrong-e.mail"
+OLD_NAME="My wrong olname"
+CORRECT_NAME="New Name"
+CORRECT_EMAIL="correct@email.address"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_NAME" = "$OLD_NAME" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
